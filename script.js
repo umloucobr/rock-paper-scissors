d925f63d1fef5785beeb1f56c66a261924c5f199
Gabriel Lucas <glucas0609@gmail.com>
2022-06-17 18:22:58 -0700

    Fixed Javascript and redid the code to fit best programming practices.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let playerScore = 0;
-let computerScore = 0;
+let g_playerScore = 0;
+let g_computerScore = 0;
+let g_score = 0;
 
 function computerPlay() {
     let choice = Math.floor((Math.random() * 3) + 1);;
-    if (choice == 1)
+    if (choice == 1) {
         return "Rock";
-    if (choice == 2)
+    }
+    else if (choice == 2) {
         return "Paper";
-    if (choice == 3)
+    }
+    else {
         return "Scissors";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -20,32 +24,34 @@ function playRound(playerSelection, computerSelection) {
         return 0;
     }
     else if (computerSelection.toUpperCase() == "SCISSORS" && playerSelection.toUpperCase() == "PAPER" || computerSelection.toUpperCase() == "ROCK" && playerSelection.toUpperCase() == "SCISSORS" || computerSelection.toUpperCase() == "PAPER" && playerSelection.toUpperCase() == "ROCK") {
-        ++computerScore;
+        ++g_computerScore;
+        --g_score;
         console.log("The Computer Won This Round!")
-        return -1;
+        return 0;
     }
     else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER" || playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS" || playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK") {
-        ++computerScore;
+        ++g_playerScore;
+        ++g_score;
         console.log("You Won This Round!")
-        return 1;
+        return 0;
     }
-    else
+    else {
     console.log("Invalid Input!");
+    }
 }
 
 function game() {
-    let score = 0;
-    for (; playerScore < 5 || computerScore < 5; playerScore++, computerScore++) 
-    {
-
+    for (let i = 0; i < 5; i++) {
         let playerSelection = window.prompt("Rock, Paper or Scissors");
         let computerSelection = computerPlay();
-        score += playRound(playerSelection, computerSelection);
+	    playRound(playerSelection, computerSelection);
     }
-        if (score > 0)
-            console.log("You won the game.");
-        else
-            console.log("You lost.");
+    if (g_score > 0) {
+        console.log("You won the game.");
+    }
+    else {
+        console.log("You lost.");
+    }
 }
 game();
 
